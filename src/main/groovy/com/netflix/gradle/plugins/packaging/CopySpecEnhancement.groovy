aec0bfcab2e847c1c5e9112faefb7259f3dfde92
Justin Ryan <jryan@netflix.com>
2013-12-12 10:22:10 -0800

    Drill into CopySpecWrapper to find the real CopySpec.

--- src/main/groovy/com/netflix/gradle/plugins/packaging/CopySpecEnhancement.groovy
+++ src/main/groovy/com/netflix/gradle/plugins/packaging/CopySpecEnhancement.groovy
@@ -1,16 +1,18 @@
 package com.netflix.gradle.plugins.packaging
 
+import org.apache.commons.lang3.reflect.FieldUtils
 import org.freecompany.redline.payload.Directive
-import org.gradle.api.internal.file.copy.CopySpecInternal
+import org.gradle.api.file.CopySpec
+import org.gradle.api.internal.file.copy.CopySpecWrapper
 
 import java.util.logging.Logger
 
 /**
  * Try to mark up CopySpec
  */
-@Category(CopySpecInternal)
+@Category(CopySpec)
 /**
- * CopySpec will nest in into() blocks, and Gradle will instantiate DefaultCopySpec itself, we have no ability to inject
+ * CopySpec will nest in into(CopySpec spec, ) blocks, and Gradle will instantiate DefaultCopySpec itself, we have no ability to inject
  * our own. When appending another copy spec to the task, it'll be created a WrapperCopySpec. In 1.8, there's a slight
  * possibility that we override addChild so we can provide our own instances, or we mod the instantiator.
  *
@@ -25,89 +27,99 @@ import java.util.logging.Logger
 class CopySpecEnhancement {
     private static final Logger LOGGER = Logger.getLogger(CopySpecEnhancement.getName())
 
-    static void user(CopySpecInternal spec, String user) {
-        spec.metaClass.user = user
+    static void appendFieldToCopySpec(CopySpec spec, String fieldName, Object value) {
+        def directSpec = spec
+        if (spec instanceof CopySpecWrapper) {
+            def delegateField = FieldUtils.getField(CopySpecWrapper, 'delegate', true)
+            directSpec = delegateField.get(spec)
+        }
+
+        LOGGER.info("Adding $fieldName to ${directSpec}")
+        directSpec.metaClass["get${fieldName.capitalize()}"] = { value }
+    }
+    static void user(CopySpec spec, String user) {
+        appendFieldToCopySpec(spec, 'user', user)
     }
 
-    static void setUser(CopySpecInternal spec, String user) {
-        spec.metaClass.user = user
+    static void setUser(CopySpec spec, String user) {
+        user(spec, user)
     }
 
-    static void permissionGroup(CopySpecInternal spec, String permissionGroup) {
-        spec.metaClass.permissionGroup = permissionGroup
+    static void permissionGroup(CopySpec spec, String permissionGroup) {
+        appendFieldToCopySpec(spec, 'permissionGroup', permissionGroup)
     }
 
-    static void setPermissionGroup(CopySpecInternal spec, String permissionGroup) {
-        spec.metaClass.permissionGroup = permissionGroup
-    }
-
-    /**
-     * RPM Only
-     */
-    static void fileType(CopySpecInternal spec, Directive fileType) {
-        spec.metaClass.fileType = fileType
-    }
-
-    /**
-     * RPM Only
-     */
-    static void setfileType(CopySpecInternal spec, Directive fileType) {
-        spec.metaClass.fileType = fileType
+    static void setPermissionGroup(CopySpec spec, String permissionGroup) {
+        permissionGroup(spec, permissionGroup)
     }
 
     /**
      * RPM Only
      */
-    static void addParentDirs(CopySpecInternal spec, boolean addParentDirs) {
-        spec.metaClass.addParentDirs = addParentDirs
+    static void fileType(CopySpec spec, Directive fileType) {
+        appendFieldToCopySpec(spec, 'fileType', fileType)
     }
 
     /**
      * RPM Only
      */
-    static void setAddParentDirs(CopySpecInternal spec, boolean addParentDirs) {
-        spec.metaClass.addParentDirs = addParentDirs
+    static void setFileType(CopySpec spec, Directive fileType) {
+        fileType(spec, fileType)
     }
 
     /**
      * RPM Only
      */
-    static void createDirectoryEntry(CopySpecInternal spec, boolean createDirectoryEntry) {
-        spec.metaClass.createDirectoryEntry = createDirectoryEntry
+    static void addParentDirs(CopySpec spec, boolean addParentDirs) {
+        appendFieldToCopySpec(spec, 'addParentDirs', addParentDirs)
     }
 
     /**
      * RPM Only
      */
-    static void setCreateDirectoryEntry(CopySpecInternal spec, boolean createDirectoryEntry) {
-        spec.metaClass.createDirectoryEntry = createDirectoryEntry
+    static void setAddParentDirs(CopySpec spec, boolean addParentDirs) {
+        addParentDirs(spec, addParentDirs)
+    }
+
+    /**
+     * RPM Only
+     */
+    static void createDirectoryEntry(CopySpec spec, boolean createDirectoryEntry) {
+        appendFieldToCopySpec(spec, 'createDirectoryEntry', createDirectoryEntry)
+    }
+
+    /**
+     * RPM Only
+     */
+    static void setCreateDirectoryEntry(CopySpec spec, boolean createDirectoryEntry) {
+        createDirectoryEntry(spec, createDirectoryEntry)
     }
 
     /**
      * DEB Only
      */
-    static void uid(CopySpecInternal spec, int uid) {
-        spec.metaClass.uid = uid
+    static void uid(CopySpec spec, int uid) {
+        appendFieldToCopySpec(spec, 'uid', uid)
     }
 
     /**
      * DEB Only
      */
-    static void setUid(CopySpecInternal spec, int uid) {
-        spec.metaClass.uid = uid
+    static void setUid(CopySpec spec, int uid) {
+        uid(spec, uid)
     }
 
     /**
      * DEB Only
      */
-    static void gid(CopySpecInternal spec, int gid) {
-        spec.metaClass.gid = gid
+    static void gid(CopySpec spec, int gid) {
+        appendFieldToCopySpec(spec, 'gid', gid)
     }
 
     /**
      * DEB Only
      */
-    static void setGid(CopySpecInternal spec, int gid) {
-        spec.metaClass.gid = gid
+    static void setGid(CopySpec spec, int gid) {
+        gid(spec, gid)
     }
 }
