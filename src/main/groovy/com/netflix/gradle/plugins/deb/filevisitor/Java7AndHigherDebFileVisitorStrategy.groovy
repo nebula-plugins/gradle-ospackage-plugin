8b8ddc6aeee6f78e2292fca11c774719d58b0378
Benjamin Muschko <benjamin.muschko@gmail.com>
2014-07-21 15:55:51 -0400

    Issue #3: Simplified logic.

--- src/main/groovy/com/netflix/gradle/plugins/deb/filevisitor/Java7AndHigherDebFileVisitorStrategy.groovy
+++ src/main/groovy/com/netflix/gradle/plugins/deb/filevisitor/Java7AndHigherDebFileVisitorStrategy.groovy
@@ -1,7 +1,5 @@
 package com.netflix.gradle.plugins.deb.filevisitor
 
-import com.netflix.gradle.plugins.deb.DataProducerDirectorySimple
-import com.netflix.gradle.plugins.deb.DataProducerFileSimple
 import com.netflix.gradle.plugins.deb.DebCopyAction
 import com.netflix.gradle.plugins.utils.JavaNIOUtils
 import org.gradle.api.file.FileCopyDetails
@@ -12,49 +10,40 @@ import java.nio.file.Path
 
 import static com.netflix.gradle.plugins.utils.FileCopyDetailsUtils.getRootPath
 
-class Java7AndHigherDebFileVisitorStrategy implements DebFileVisitorStrategy {
-    private final List<DataProducer> dataProducers
-    private final List<DebCopyAction.InstallDir> installDirs
-
+class Java7AndHigherDebFileVisitorStrategy extends AbstractDebFileVisitorStrategy {
     Java7AndHigherDebFileVisitorStrategy(List<DataProducer> dataProducers, List<DebCopyAction.InstallDir> installDirs) {
-        this.dataProducers = dataProducers
-        this.installDirs = installDirs
+        super(dataProducers, installDirs)
     }
 
     @Override
     void addFile(FileCopyDetails fileDetails, File source, String user, int uid, String group, int gid, int mode) {
-        String rootPath = getRootPath(fileDetails)
-
         try {
             if(!JavaNIOUtils.isSymbolicLink(fileDetails.file.parentFile)) {
-                dataProducers << new DataProducerFileSimple(rootPath, source, user, uid, group, gid, mode)
+                addProducerFile(fileDetails, source, user, uid, group, gid, mode)
             }
         }
         catch(UnsupportedOperationException e) {
             // For file details that have filters, accessing the file throws this exception
-            dataProducers << new DataProducerFileSimple(rootPath, source, user, uid, group, gid, mode)
+            addProducerFile(fileDetails, source, user, uid, group, gid, mode)
         }
     }
 
     @Override
     void addDirectory(FileCopyDetails dirDetails, String user, int uid, String group, int gid, int mode) {
-        String rootPath = getRootPath(dirDetails)
         boolean symbolicLink = JavaNIOUtils.isSymbolicLink(dirDetails.file)
 
         if(symbolicLink) {
-            Path path = JavaNIOUtils.createPath(dirDetails.file.path)
-            Path target = JavaNIOUtils.readSymbolicLink(path)
-            dataProducers << new DataProducerLink(rootPath, target.toFile().path, true, null, null, null)
+            addProducerLink(dirDetails)
         }
         else {
-            dataProducers << new DataProducerDirectorySimple(rootPath, user, uid, group, gid, mode)
-
-            // addParentDirs is implicit in jdeb, I think.
-            installDirs << new DebCopyAction.InstallDir(
-                    name: rootPath,
-                    user: user,
-                    group: group,
-            )
+            addProducerDirectoryAndInstallDir(dirDetails, user, uid, group, gid, mode)
         }
+    }
+
+    private void addProducerLink(FileCopyDetails details) {
+        String rootPath = getRootPath(details)
+        Path path = JavaNIOUtils.createPath(details.file.path)
+        Path target = JavaNIOUtils.readSymbolicLink(path)
+        dataProducers << new DataProducerLink(rootPath, target.toFile().path, true, null, null, null)
     }
 }
--- src/main/groovy/com/netflix/gradle/plugins/deb/filevisitor/PreJava7DebFileVisitorStrategy.groovy
+++ src/main/groovy/com/netflix/gradle/plugins/deb/filevisitor/PreJava7DebFileVisitorStrategy.groovy
@@ -1,37 +1,21 @@
 package com.netflix.gradle.plugins.deb.filevisitor
 
-import com.netflix.gradle.plugins.deb.DataProducerDirectorySimple
-import com.netflix.gradle.plugins.deb.DataProducerFileSimple
 import com.netflix.gradle.plugins.deb.DebCopyAction
 import org.gradle.api.file.FileCopyDetails
 import org.vafer.jdeb.DataProducer
 
-import static com.netflix.gradle.plugins.utils.FileCopyDetailsUtils.getRootPath
-
-class PreJava7DebFileVisitorStrategy implements DebFileVisitorStrategy {
-    private final List<DataProducer> dataProducers
-    private final List<DebCopyAction.InstallDir> installDirs
-
+class PreJava7DebFileVisitorStrategy extends AbstractDebFileVisitorStrategy {
     PreJava7DebFileVisitorStrategy(List<DataProducer> dataProducers, List<DebCopyAction.InstallDir> installDirs) {
-        this.dataProducers = dataProducers
-        this.installDirs = installDirs
+        super(dataProducers, installDirs)
     }
 
     @Override
     void addFile(FileCopyDetails fileDetails, File source, String user, int uid, String group, int gid, int mode) {
-        dataProducers << new DataProducerFileSimple(getRootPath(fileDetails), source, user, uid, group, gid, mode)
+        addProducerFile(fileDetails, source, user, uid, group, gid, mode)
     }
 
     @Override
     void addDirectory(FileCopyDetails dirDetails, String user, int uid, String group, int gid, int mode) {
-        String rootPath = getRootPath(dirDetails)
-        dataProducers << new DataProducerDirectorySimple(rootPath, user, uid, group, gid, mode)
-
-        // addParentDirs is implicit in jdeb, I think.
-        installDirs << new DebCopyAction.InstallDir(
-                name: rootPath,
-                user: user,
-                group: group,
-        )
+        addProducerDirectoryAndInstallDir(dirDetails, user, uid, group, gid, mode)
     }
 }
