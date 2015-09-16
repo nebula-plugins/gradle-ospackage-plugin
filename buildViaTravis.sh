f450a979ce2dc703c438650a35fdc90b0c27785b
Danny Thomas <dannyt@netflix.com>
2015-09-16 11:52:51 -0700

    Add --info to build commands

--- buildViaTravis.sh
+++ buildViaTravis.sh
@@ -3,10 +3,10 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  ./gradlew --stacktrace build
+  ./gradlew --stacktrace --info build
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-  ./gradlew --stacktrace -Prelease.travisci=true snapshot
+  ./gradlew --stacktrace --info -Prelease.travisci=true snapshot
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
   case "$TRAVIS_TAG" in
@@ -19,5 +19,5 @@ elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   esac
 else
   echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
-  ./gradlew --stacktrace build
+  ./gradlew --stacktrace --info build
 fi
