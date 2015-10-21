cae9779a4aab0fa780c26b9df0ed548682c16f56
Danny Thomas <dannyt@netflix.com>
2015-10-21 15:11:07 -0700

    Update all build types to debug

--- buildViaTravis.sh
+++ buildViaTravis.sh
@@ -6,18 +6,18 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   ./gradlew --stacktrace --debug build
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-  ./gradlew --stacktrace --info -Prelease.travisci=true snapshot
+  ./gradlew --stacktrace --debug -Prelease.travisci=true snapshot
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
   case "$TRAVIS_TAG" in
   *-rc\.*)
-    ./gradlew --stacktrace --info -Prelease.travisci=true -Prelease.useLastTag=true candidate
+    ./gradlew --stacktrace --debug -Prelease.travisci=true -Prelease.useLastTag=true candidate
     ;;
   *)
-    ./gradlew --stacktrace --info -Prelease.travisci=true -Prelease.useLastTag=true final publishPlugins
+    ./gradlew --stacktrace --debug -Prelease.travisci=true -Prelease.useLastTag=true final publishPlugins
     ;;
   esac
 else
   echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
-  ./gradlew --stacktrace --info build
+  ./gradlew --stacktrace --debug build
 fi
