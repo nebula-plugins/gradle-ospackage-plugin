82b466a0da64ff0f288f76d3d3fcab6ec8357dfb
Danny Thomas <dannyt@netflix.com>
2015-10-21 15:20:36 -0700

    Set logging back to info level by default

--- buildViaTravis.sh
+++ buildViaTravis.sh
@@ -3,21 +3,21 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  ./gradlew --stacktrace --debug build
+  ./gradlew --stacktrace --info build
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-  ./gradlew --stacktrace --debug -Prelease.travisci=true snapshot
+  ./gradlew --stacktrace --info -Prelease.travisci=true snapshot
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
   case "$TRAVIS_TAG" in
   *-rc\.*)
-    ./gradlew --stacktrace --debug -Prelease.travisci=true -Prelease.useLastTag=true candidate
+    ./gradlew --stacktrace --info -Prelease.travisci=true -Prelease.useLastTag=true candidate
     ;;
   *)
-    ./gradlew --stacktrace --debug -Prelease.travisci=true -Prelease.useLastTag=true final publishPlugins
+    ./gradlew --stacktrace --info -Prelease.travisci=true -Prelease.useLastTag=true final publishPlugins
     ;;
   esac
 else
   echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
-  ./gradlew --stacktrace --debug build
+  ./gradlew --stacktrace --info build
 fi
