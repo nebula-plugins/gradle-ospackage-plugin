e95b4b7312bd12f73e4476ec29f7285fee031849
bi- <bi-@freemail.hu>
2015-12-29 21:58:29 +0000

    Debian directory() consturct support, based on tbroyer idea (DataProducerPathTemplate from JDeb). https://github.com/nebula-plugins/gradle-ospackage-plugin/issues/115

--- src/test/groovy/com/netflix/gradle/plugins/deb/DebPluginTest.groovy
+++ src/test/groovy/com/netflix/gradle/plugins/deb/DebPluginTest.groovy
@@ -963,4 +963,25 @@ class DebPluginTest extends ProjectSpec {
         '1.0.0-rc.1'         | '1.0.0~rc.1'
         '1.0.0-dev.3+abc219' | '1.0.0~dev.3'
     }
+
+    @Issue("https://github.com/nebula-plugins/gradle-ospackage-plugin/issues/115")
+    def 'directory construct'() {
+        given:
+        project.apply plugin: 'nebula.deb'
+        Deb debTask = project.task('buildDeb', type: Deb) {
+            user 'test'
+            permissionGroup 'testgroup'
+            directory("/var/log/customemptyfolder", 0750)
+        }
+
+        when:
+        debTask.execute()
+
+        then:
+        def scan = new Scanner(debTask.archivePath)
+        def emptydir = scan.getEntry('./var/log/customemptyfolder/')
+        emptydir.userName == 'test'
+        emptydir.groupName == 'testgroup'
+        emptydir.mode == 0750
+    }
 }
