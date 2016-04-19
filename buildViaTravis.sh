2c4195111b7468970c015e666bb2b3e385dfddca
Danny Thomas <dannyt@netflix.com>
2016-04-19 12:34:27 -0700

    Update buildViaTravis script to include --info and --stacktrace for every build

--- buildViaTravis.sh
+++ buildViaTravis.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 # This script will build the project.
 
+SWITCHES="--info --stacktrace"
+
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  ./gradlew --stacktrace --info build
+  ./gradlew build $SWITCHES
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-  ./gradlew --stacktrace --info -Prelease.travisci=true snapshot
+  ./gradlew -Prelease.travisci=true snapshot $SWITCHES
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
   case "$TRAVIS_TAG" in
   *-rc\.*)
-    ./gradlew --stacktrace --info -Prelease.travisci=true -Prelease.useLastTag=true candidate
+    ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true candidate $SWITCHES
     ;;
   *)
-    ./gradlew --stacktrace --info -Prelease.travisci=true -Prelease.useLastTag=true final publishPlugins
+    ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true final publishPlugins $SWITCHES
     ;;
   esac
 else
   echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
-  ./gradlew --stacktrace --info build
+  ./gradlew build $SWITCHES
 fi
