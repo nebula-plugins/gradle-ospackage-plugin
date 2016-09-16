01c854c93a061adcc01dc5614ed61264974377ca
Sigmar Walter <sigmar.walter@comsoft.aero>
2016-09-16 17:25:52 +0200

    exttended RPM directory() with owner, group and addParents parameters (#206)

--- src/main/groovy/com/netflix/gradle/plugins/packaging/Directory.groovy
+++ src/main/groovy/com/netflix/gradle/plugins/packaging/Directory.groovy
@@ -3,4 +3,7 @@ package com.netflix.gradle.plugins.packaging
 class Directory {
     String path
     int permissions = -1
+    String user = null
+    String permissionGroup = null
+    boolean addParents = false
 }
