b8d4450231c90222a7024241f5a2c1340a3f8777
Martin Chalupa <mchalupa@netflix.com>
2018-12-18 16:16:58 -0800

    Allow specification of custom default values for daemons

---
+++ src/main/groovy/com/netflix/gradle/plugins/daemon/DefaultDaemonDefinitionExtension.groovy
@@ -0,0 +1,5 @@
+package com.netflix.gradle.plugins.daemon
+
+class DefaultDaemonDefinitionExtension extends DaemonDefinition {
+    boolean useExtensionDefaults = false
+}
--- src/main/groovy/com/netflix/gradle/plugins/daemon/OspackageDaemonPlugin.groovy
+++ src/main/groovy/com/netflix/gradle/plugins/daemon/OspackageDaemonPlugin.groovy
@@ -31,7 +31,9 @@ class OspackageDaemonPlugin implements Plugin<Project> {
     Project project
     DaemonExtension extension
     DaemonTemplatesConfigExtension daemonTemplatesConfigExtension
+    DefaultDaemonDefinitionExtension defaultDefinition
     private final CollectionCallbackActionDecorator collectionCallbackActionDecorator
+
 
     private final String DEFAULT_TEMPLATES_FOLDER = '/com/netflix/gradle/plugins/daemon'
 
@@ -64,6 +66,7 @@ class OspackageDaemonPlugin implements Plugin<Project> {
         DomainObjectCollection<DaemonDefinition> daemonsList = factory.create(DaemonDefinition)
         extension = project.extensions.create('daemons', DaemonExtension, daemonsList)
         daemonTemplatesConfigExtension = project.extensions.create('daemonsTemplates', DaemonTemplatesConfigExtension)
+        defaultDefinition = project.extensions.create('daemonsDefaultDefinition', DefaultDaemonDefinitionExtension)
 
         // Add daemon to project
         project.ext.daemon = { Closure closure ->
@@ -147,6 +150,9 @@ class OspackageDaemonPlugin implements Plugin<Project> {
     }
 
     def getDefaultDaemonDefinition(boolean isRedhat) {
-        new DaemonDefinition(null, null, 'root', 'multilog t ./main', "./main", "nobody", isRedhat ? [3, 4, 5] : [2, 3, 4, 5], Boolean.TRUE, 85, 15)
+        if (defaultDefinition.useExtensionDefaults)
+            defaultDefinition
+        else
+            new DaemonDefinition(null, null, 'root', 'multilog t ./main', "./main", "nobody", isRedhat ? [3, 4, 5] : [2, 3, 4, 5], Boolean.TRUE, 85, 15)
     }
 }
