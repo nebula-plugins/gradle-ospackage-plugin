da89a023f672afb5da38fc5fd54baaa927487515
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2019-08-26 10:04:26 -0700

    DataProducerDirectorySimple: support for TarArchive in jdeb 1.8

--- src/main/groovy/com/netflix/gradle/plugins/deb/DataProducerDirectorySimple.groovy
+++ src/main/groovy/com/netflix/gradle/plugins/deb/DataProducerDirectorySimple.groovy
@@ -3,6 +3,7 @@ package com.netflix.gradle.plugins.deb
 import groovy.transform.Canonical
 import org.vafer.jdeb.DataConsumer
 import org.vafer.jdeb.DataProducer
+import org.vafer.jdeb.shaded.commons.compress.archivers.tar.TarArchiveEntry
 
 @Canonical
 public class DataProducerDirectorySimple implements DataProducer {
@@ -17,6 +18,16 @@ public class DataProducerDirectorySimple implements DataProducer {
     @Override
     void produce(DataConsumer receiver) throws IOException {
         // TODO Investigate what happens if we don't have a uid/gid
-        receiver.onEachDir(dirname, null, user, uid, group, gid, mode, 0)
+        receiver.onEachDir(createEntry())
+    }
+
+    private TarArchiveEntry createEntry() {
+        TarArchiveEntry entry = new TarArchiveEntry(dirname)
+        entry.userName = user
+        entry.userId = uid
+        entry.groupName = group
+        entry.groupId = gid
+        entry.mode = mode
+        entry
     }
 }
