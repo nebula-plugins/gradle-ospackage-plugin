ef96d5b0abf8a1f1109bb5569e334c16da5ed632
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2019-11-04 14:03:28 -0800

    Revert "Revert "tasks.withType: use configureAction""
    This reverts commit eaa47d8927ae072cc58d776501c4924fe523da06.

--- src/main/groovy/com/netflix/gradle/plugins/application/OspackageApplicationPlugin.groovy
+++ src/main/groovy/com/netflix/gradle/plugins/application/OspackageApplicationPlugin.groovy
@@ -20,6 +20,7 @@ import com.netflix.gradle.plugins.deb.Deb
 import com.netflix.gradle.plugins.packaging.ProjectPackagingExtension
 import com.netflix.gradle.plugins.packaging.SystemPackagingPlugin
 import com.netflix.gradle.plugins.rpm.Rpm
+import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
@@ -61,9 +62,12 @@ class OspackageApplicationPlugin implements Plugin<Project> {
             packagingExt.into(extension.getPrefix())
 
             [Deb, Rpm].each { Class type ->
-                project.tasks.withType(type) { Task task ->
-                    task.dependsOn(installTask)
-                }
+                project.tasks.withType(type).configureEach(new Action<Task>() {
+                    @Override
+                    void execute(Task task) {
+                        task.dependsOn(installTask)
+                    }
+                })
             }
         }
     }
