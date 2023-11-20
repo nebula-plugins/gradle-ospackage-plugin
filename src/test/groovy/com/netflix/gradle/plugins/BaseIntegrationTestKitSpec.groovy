dd267ee614440920d67110a56fe88b204256cee4
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2023-11-20 15:19:34 -0800

    OspackageApplicationDaemonPluginLauncherSpec: use test kit

---
+++ src/test/groovy/com/netflix/gradle/plugins/BaseIntegrationTestKitSpec.groovy
@@ -0,0 +1,9 @@
+package com.netflix.gradle.plugins
+
+import nebula.test.IntegrationTestKitSpec
+
+abstract class BaseIntegrationTestKitSpec extends IntegrationTestKitSpec {
+    def setup() {
+
+    }
+}
